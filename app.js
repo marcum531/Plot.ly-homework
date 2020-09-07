4c9c7fefee70c17584121564cf10463b65f00437
Nicole (Deborah) Marcum <marcum531@gmail.com>
2020-09-06 20:13:24 -0400

    second upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function dataset(data) {
-    d3.json('Plot.ly-homework/samples.json').then(function(sampleData){
+    d3.json(`/metadata/${sample}`).then(function(sampleData){
         console.log(sampleData);
 
         var panel = d3.select("#sample-metadata");
@@ -13,30 +13,75 @@ function dataset(data) {
 }
 
 function buildCharts(data) {
-    d3.json('Plot.ly-homework/samples.json').then(function(sampleData) {
+    d3.json(`/samples/${sample}`).then(function(sampleData) {
         console.log(sampleData);
 
         const otu_ids = sampleData.otu_ids;
         const out_labels = sampleData.out_labels;
         const sample_values = sampleData.sample_values;
 
-        var circleData = [{
-            x: otu_ids,
-            y: sample_values,
-            text: out_labels,
-            mode: 'markers',
+        var barData = [{
+            x: sample_values,
+            y: otu_ids,
+            type: 'bar',
+            text: otu_labels,
             marker: {
-                size: sample_values,
-                color: otu_ids,
+                color: 'rgb(142, 124, 195)'
             }
         }];
 
-        var circleLayout = {
+        var barLayout = {
+            hovermode: 'closest',
+            yaxis: {title: 'OTU ID', tickangle: -180},
+            xaxis: {title: 'Sample Values'},
+            bargap: 0.05
+        };
+
+        Plotly.newplot('myDiv', barData, barLayout);
+
+        var bubbleData = [{
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: 'markers',
+            marker: {
+                colorscale: 'sunset',
+                size: sample_values,
+                color: otu_ids 
+            }
+        }];
+
+        var bubbleLayout = {
             margin: {t: 0},
             hovermode: 'closest',
             xaxis: {title: 'OTU ID'}
         };
 
-        Plotly.plot('bubble', circleData, circleLayout);
-    })
+        Plotly.plot('bubble', bubbleData, bubbleLayout);
+
+    });
 }
+
+function init() {
+    var selector = d3.select("#selDataset");
+
+    d3.json("/samples/${names}").then((sampleNames) => {
+        sampleNames.forEach((sample) => {
+            selector.append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+
+    const sampleOne = sampleNames[0];
+    buildCharts(sampleOne);
+    dataset(sampleOne);
+
+    });
+}
+
+function  options(sampleNew) {
+    buildCharts(sampleNew);
+    dataset(sampleNew);
+}
+
+init();
